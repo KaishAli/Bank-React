ed8f10c14a7c93e37c38e2b0eb90ca2d7f9f6b99
KaishAli <360mohdkaishali.cs@gmail.com>
2022-03-06 16:58:14 +0530

    Updates

---
+++ precache-manifest.26b510af01d61e26d3103a5260d1b243.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "744dc57fc54d0e53ef67",
+    "url": "/Bank-React/static/css/main.cfb00d71.chunk.css"
+  },
+  {
+    "revision": "744dc57fc54d0e53ef67",
+    "url": "/Bank-React/static/js/main.744dc57f.chunk.js"
+  },
+  {
+    "revision": "213912c3d00d5092cf6f",
+    "url": "/Bank-React/static/css/1.69a4ab3c.chunk.css"
+  },
+  {
+    "revision": "213912c3d00d5092cf6f",
+    "url": "/Bank-React/static/js/1.213912c3.chunk.js"
+  },
+  {
+    "revision": "2467f7fce6f5da51edeb",
+    "url": "/Bank-React/static/js/runtime~main.2467f7fc.js"
+  },
+  {
+    "revision": "4eb253c07061c934764504465102e40e",
+    "url": "/Bank-React/static/media/index.4eb253c0.mjs"
+  },
+  {
+    "revision": "758df62763311be99f1d3ca19db02aa6",
+    "url": "/Bank-React/index.html"
+  }
+];
